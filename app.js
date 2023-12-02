989330f66de7e365537ebeab23950180ad1adf01
habibjeux <habibjeux@gmail.com>
2023-12-02 10:59:00 +0000

    firts interaction with the database

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const app = express();
+const Thing = require('./models/thing');
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb+srv://htjeux:JZYO24ZuSdz9OCBs@cluster0.xebjmhn.mongodb.net/?retryWrites=true&w=majority',
@@ -19,32 +20,19 @@ app.use((_, res, next) => {
 });
 
 app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-      message: 'Objet créé !'
+    delete req.body._id;
+    const thing = new Thing({
+      ...req.body
     });
+    thing.save()
+      .then(() => res.status(201).json({ message : 'Objet enregistré !'}))
+      .catch(error => res.status(400).json({ error }));
 });
 
 app.use('/api/stuff', (_, res) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff);
-  });
+   Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch(errot => res.status(400).json({ error }));
+});
 
 module.exports = app;
